1645df7f14dfb989ec379fb0a13baf43e34a7d45
MathiasWeberComCave <q-mwb@gmx.de>
2022-11-09 12:26:06 +0100

    Verschiedene Arten eine Summe zu berechnen

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,23 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+// Deklaration
+
+// let zahl1 = +(prompt ("Zahl eingeben:")) //es kann auch Number() oder parseInt() verwendet werden
+// let zahl2 = +(prompt ("Zahl eingeben:"))
+
+// Berechnung
+
+// console.log ("Die Summe der Zahlen ist: " + (zahl1 + zahl2).toFixed(2)) //Ausgabe der Summe auf zwei Kommastellen gerundet
+
+
+
+//eine Variante
+// let zahl1 = parseFloat(prompt ("Zahl eingeben:"))
+// let zahl2 = parseFloat(prompt ("Zahl eingeben:"))
+
+// console.log ("Die Summe der Zahlen ist: " + zahl1 + zahl2)
+
+//Die minmalistische Variante
+
+console.log(+(prompt ("Zahl:")) + +(prompt ("Zahl:"))) // Nachteil, keine Inputüberprüfung
